63290d8e07d956872bee09cf36d089eb1e28a064
Jay Winstead <jwinstead@launchdarkly.com>
2018-04-19 15:08:34 -0700

    2.0.1 automatically generated from 4e228d4

--- _client.sh
+++ _client.sh
@@ -10,7 +10,7 @@
 # !
 # ! Based on: https://github.com/Valodim/zsh-curl-completion/blob/master/_curl
 # !
-# ! Generated on: 2018-04-19T15:06:49.109-07:00
+# ! Generated on: 2018-04-19T15:08:03.520-07:00
 # !
 # !
 # ! Installation:
